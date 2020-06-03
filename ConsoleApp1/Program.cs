cff7b0d9abdc9788c202388958813686bee7fdab
Vivian Tavares <viviaan.tavares@gmail.com>
2020-06-03 18:05:14 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
